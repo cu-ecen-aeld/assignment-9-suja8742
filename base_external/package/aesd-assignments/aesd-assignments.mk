df1dd728f33359080766d04179a61550d36d7f2b
suja8742 <suja8742@colorado.edu>
2023-04-04 14:19:57 -0600

    Fulltest passed

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'fe64475747eebdcce2888cd31d7c99dc97a5c179'
+AESD_ASSIGNMENTS_VERSION = '6531452b4b4feea0c87c5678294f223ffb9a042d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesd-chardriver/aesd-chardriver.mk
+++ base_external/package/aesd-chardriver/aesd-chardriver.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHARDRIVER_VERSION = 'fe64475747eebdcce2888cd31d7c99dc97a5c179'
+AESD_CHARDRIVER_VERSION = '6531452b4b4feea0c87c5678294f223ffb9a042d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,7 +15,7 @@ AESD_CHARDRIVER_SITE_METHOD = git
 AESD_CHARDRIVER_GIT_SUBMODULES = YES
 
 AESD_CHARDRIVER_MODULE_SUBDIRS += aesd-char-driver/
-
+AESD_CHARDRIVER_OVERRIDE_SRCDIR = /home/suja8742/Documents/aesd_ass/a7/assignments-3-and-later-suja8742/
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_CHARDRIVER_INSTALL_TARGET_CMDS
