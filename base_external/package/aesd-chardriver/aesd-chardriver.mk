2e187b540e982864352045c4601ed864a9367f61
Sudarshan Jagannathan <suja8742@aesd-students.colorado.edu>
2023-04-05 05:07:36 +0000

    mybad

--- base_external/package/aesd-chardriver/aesd-chardriver.mk
+++ base_external/package/aesd-chardriver/aesd-chardriver.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHARDRIVER_VERSION = '6531452b4b4feea0c87c5678294f223ffb9a042d'
+AESD_CHARDRIVER_VERSION = '015890e08ddc107ab060605bf9fea6df74ac82b7'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,7 +15,6 @@ AESD_CHARDRIVER_SITE_METHOD = git
 AESD_CHARDRIVER_GIT_SUBMODULES = YES
 
 AESD_CHARDRIVER_MODULE_SUBDIRS += aesd-char-driver/
-AESD_CHARDRIVER_OVERRIDE_SRCDIR = /home/suja8742/Documents/aesd_ass/a7/assignments-3-and-later-suja8742/
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_CHARDRIVER_INSTALL_TARGET_CMDS
